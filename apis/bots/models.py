47ee7049f4eff6621bf6fe261993a9601bf09fe6
Sklyar13OFF <dansklyarov13@gmail.com>
2026-02-15 19:51:50 +0100

    Update models.py

--- apis/bots/models.py
+++ apis/bots/models.py
@@ -391,6 +391,7 @@ class ObvIndicator(BaseIndicator):
 
 class EmaIndicator(BaseIndicator):
     bot = models.ForeignKey(Bot, related_name="ema_indicators", on_delete=models.CASCADE)
+    period = models.IntegerField(default=14)
 
     class Meta:
         verbose_name = "EMA Indicator"
